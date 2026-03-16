96b0cabff35785cea96623086c80181fd8bf44f1
didier-tech <didier@ascingenieriasaesp.com>
2026-03-16 17:48:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+import psycopg2
 import os
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+def get_db():
+    conn = psycopg2.connect(DATABASE_URL)
+    return conn
 import json
 import time
 import tempfile
