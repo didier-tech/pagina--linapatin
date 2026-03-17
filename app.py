0362af3bada74bd1337e8530681b8ba54508cc7d
didier-tech <didier@ascingenieriasaesp.com>
2026-03-17 17:00:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 
 import os
 import psycopg2
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def get_db():
-    return psycopg2.connect(DATABASE_URL) # ← ESTA LÍNEA VA INDENTADA
+    return psycopg2.connect(
+        DATABASE_URL,
+        sslmode='require'
+    )
 
 @app.route("/")
 def index():
@@ -16,29 +19,28 @@ def index():
         conn = get_db()
         cur = conn.cursor()
 
-        cur.execute("SELECT titulo, contenido FROM noticias ORDER BY fecha DESC")
-        noticias_db = cur.fetchall()
+        cur.execute("SELECT id, titulo, contenido, fecha FROM noticias ORDER BY fecha DESC")
+        datos = cur.fetchall()
 
         cur.close()
         conn.close()
-        # convertir a formato tipo JSON (como antes)
+
         noticias = []
-        for n in noticias_db:
+        for n in datos:
             noticias.append({
                 "id": n[0],
                 "title": n[1],
                 "content": n[2],
-                "created_at": n[3]
-            })
-                
-
-
-        return render_template("index.html", noticias=noticias, heroes[])
+                "created_at": n[3],
+                "image": None
+         })
 
     except Exception as e:
-        print("ERROR:", e)
-        return "Error en la base de datos"
+         print("ERROR BD:", e)
+         noticias = []
+
+    # SIEMPRE responde (evita 502)
+    return render_template("index.html", noticias=noticias, heroes=[])
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
