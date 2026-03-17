b47460a0db8e6ef999ccfe54afbba30568734012
didier-tech <didier@ascingenieriasaesp.com>
2026-03-17 12:33:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,5 @@ def index():
         return "Error en la base de datos"
 
 if __name__ == "__main__":
-port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
